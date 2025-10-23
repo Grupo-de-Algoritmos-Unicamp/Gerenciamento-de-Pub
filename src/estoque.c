2681ec23219b27b0c0883284dd7e04e65ebb74c4
Sabrina Lima <sbflima7@gmail.com>
2025-10-23 10:13:46 -0300

    Alteração e adição de funções
    Alteração da função cadastrarProduto(): alteração na comparação das escolhas do usuário.
    Adição das funções: excluirProduto(), alterarProduto() e menuAlterarProduto().

--- src/estoque.c
+++ src/estoque.c
@@ -32,28 +32,118 @@ void cadastrarProduto(){
     fclose(arquivo);
 }
 
-void menuCadastroProduto(){
-    int resposta = 1;
-    
-    // Repete a pergunta e cadastra produtos no estoque e 
-    // volta para o menu inicial quando o usuário digita 0
-    while (resposta != 0) {
-        printf("\n\n-------Menu de Cadastro-------\n");
-        printf("Digite 1 para cadastrar um produto\n");
-        printf("Digite 0 para sair\n");
-        printf("Resposta: ");
-        scanf("%d", &resposta);
-        
-        if(resposta == 1){
-            cadastrarProduto();
-        } else if(resposta == 0){
-            printf("Saindo...\n");
-            break;
-        } else{
-            printf("Resposta inválida, digite novamente.\n");
+void menuAlterarProduto(char *nome, float *preco, char *tipo, int *quantidade) {
+    int opcao=0;
+    do{
+        printf("\n\n--- Produto Encontrado ---\n");
+        printf("Tipo: %c\n", *tipo);
+        printf("Nome: %s\n", nome);
+        printf("Preço: R$%.2f\n", *preco);
+        printf("Quantidade: %d\n", *quantidade);
+        printf("--------------------------\n");
+        printf("O que deseja alterar?\n");
+        printf("1. Nome\n2. Preço\n3. Tipo de produto\n4. Quantidade no estoque\n0. Salvar alterações e Voltar ao menu\n");
+        printf("Opção: ");
+        scanf("%d", &opcao);
+
+        switch(opcao){
+            case 1:
+                printf("Digite o novo nome: ");
+                scanf(" %[^\n]", nome); 
+                break;
+            case 2:
+                printf("Digite o novo preço: ");
+                scanf(" %f", preco); 
+                break;
+            case 3:
+                printf("Digite o novo tipo de produto: ");
+                scanf(" %c", tipo);
+                break;
+            case 4:
+                printf("Digite a nova quantidade do produto: ");
+                scanf(" %d", quantidade);
+                break;
+            case 0:
+                printf("Alterações salvas.\n");
+                break;
+            default:
+                printf("Opção inválida\n");
+                break;
         }
+    }while(opcao!=0);
+}
+
+void alterarProduto(){
+    int codigoAlterar;
+    int encontrado = 0;
+    
+    int codigo;
+    char tipo;
+    char nome[50];
+    float preco;
+    int quantidade;
+
+    FILE *arquivo=fopen("estoque.txt", "r");
+    FILE *temp=fopen("temp.txt", "w");
+    //Verifica se os arquivos podem ser abertos.
+    if((arquivo==NULL)||(temp==NULL)){
+        printf("Erro: não é possível abrir o arquivo\n");
+        return;
+    }
+    
+    printf("\n\n-------Alterar Produto-------\n");
+    printf("Digite o código do produto que deseja alterar: ");
+    scanf("%d", &codigoAlterar);
+    
+    //Percorre o arquivo de texto buscando pelo produto que possui o código
+    while(fscanf(arquivo, "%d %c %s %f %d", &codigo, &tipo, nome, &preco, &quantidade)!=EOF){
+        if(codigo==codigoAlterar){
+            encontrado=1;
+            //Chama a função menuAlterarProduto para alterar o arquivo
+            menuAlterarProduto(nome, &preco, &tipo, &quantidade);
+        }
+        // Escreve a linha no arquivo temporário.
+        fprintf(temp, "%d %c %s %.2f %d\n", codigo, tipo, nome, preco, quantidade);
     }
 
+    fclose(arquivo);
+    fclose(temp);
+    
+    if(encontrado){
+        //Se o produto foi encontrado, apaga 'estoque.txt', e renomeia 'temp.txt' para ser o novo 'estoque.txt'
+        remove("estoque.txt");
+        rename("temp.txt", "estoque.txt");
+        printf("\nProduto alterado com sucesso!\n");
+    }else{
+        //Se não foi encontrado o produto, apenas apaga o arquivo temporário que foi criado.
+        remove("temp.txt");
+        printf("\nProduto com código %d não encontrado.\n", codigoAlterar);
+    }
+}
+
+void excluirProduto(){
+
+}
+
+void menuCadastroProduto(){
+    int resposta=3;
+    void (*gerenciar[])()={menuInicial, cadastrarProduto, alterarProduto, excluirProduto};
+    //Repete a pergunta e cadastra produtos no estoque e 
+    //volta para o menu inicial quando o usuário digita 0
+    while (resposta!=0) {
+        printf("\n\n-------Menu de Cadastro-------\n");
+        printf("Digite 1 para cadastrar um produto\n");
+        printf("Digite 2 para alterar o produto\n");//Alteração do código
+        printf("Digite 3 para excluir um peoduto no estoque\n");//Alteração do código
+        printf("Digite 0 para voltar ao menu inicial\n");//Ateração no código
+        printf("Resposta: ");
+        scanf("%d", &resposta);
+        if(resposta<=3){//Alteração do código
+            gerenciar[resposta]();//Alteração do código
+        } else{//Alteração do código
+            printf("Resposta inválida\n");//Alteração do código
+        }
+    }
 }
 
 //CONSULTA DE PRODUTOS (LISTA)---------------------------------------------------------------------------------------------------
@@ -163,4 +253,5 @@ void menuConsultarProdutos() {
             break;
     }
 }
+
 
