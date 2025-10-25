3cdc3f68a91b262b1ccaba8670b69d74e7bfc1a8
Arthur Luiz Lopes Araujo <arthur.luizlopesaraujo@gmail.com>
2025-10-25 19:31:28 -0300

    Add payment processing and order file generation functions

--- src/pedido.h
+++ src/pedido.h
@@ -1,6 +1,8 @@
 #ifndef PEDIDO_H
 #define PEDIDO_H
 
+int processarPagamento(float valorTotal, float *pagoCliente, float *troco);
+void gerarArquivoPedido(char cpf[], int numeroPedido, char nome[][31], float preco[], int quantidade[], int i, float valorTotal, int tipoPagamento, float pagoCliente, float troco);
 void registrarPedido();
 
 #endif
