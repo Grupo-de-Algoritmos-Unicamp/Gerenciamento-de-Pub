0e37537defa767625489dbdb70ea7da7a74d5594
Arthur Luiz Lopes Araujo <arthur.luizlopesaraujo@gmail.com>
2025-10-28 13:57:34 -0300

    Correção da ordem das funções e da função menuInicial

--- src/utils.c
+++ src/utils.c
@@ -3,27 +3,28 @@
 #include "estoque.h"
 #include "pedido.h"
 
-void menuInicial(){
-    int resposta;
-    void (*gerenciar[])()={sair, menuCadastroProduto, menuConsultarProdutos, /*cadastro de clientes*/, registrarPedido /*gerar relatório*/); 
-    while (resposta!=0){
-    printf("--------------MENU INICIAL--------------");
-    printf("\n O que deseja fazer?");
-    printf("\n(1) Cadastrar produto\n(2) Consultar produto\n(3) Cadastrar cliente\n(4) Registrar pedido\n(5) Gerar relatório\n(0) Sair");
-    printf("\nSelecionar: ");
-    scanf("%d",&resposta);
-    printf("\n----------------------------------------");
-    switch(resposta){
-        if(resposta<=3 && resposta>=0){
-            gerenciar[resposta]();
-        }else{
-            printf("Resposta inválida\n");
-            }
-        }
-    }
-}
-
 void sair(){
     printf("Saindo...");
 }
+
+void menuInicial(){
+    int resposta;
+    void (*gerenciar[])() = {sair, menuCadastroProduto, menuConsultarProdutos, registrarPedido};
+    
+    while (resposta!=0){
+        printf("--------------MENU INICIAL--------------");
+        printf("\n O que deseja fazer?");
+        printf("\n(1) Cadastrar produto\n(2) Consultar produto\n(3) Cadastrar cliente\n(4) Registrar pedido\n(5) Gerar relatório\n(0) Sair");
+        printf("\nSelecionar: ");
+        scanf("%d",&resposta);
+        printf("\n----------------------------------------");
+
+        if (resposta >= 0 && resposta <= 3) {
+            gerenciar[resposta](); 
+        } else {
+            printf("Opção inválida!\n");
+        }  
+    }
+}
+
 
