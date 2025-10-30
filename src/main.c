7dd15ed2c6cd0da15a0884f55ee949e87e9fe0b5
BeatrizAmaral-251006 <beatriz.amaral.2006@gmail.com>
2025-10-30 09:34:09 -0300

    Caracteres especiais
    Adição da linha system("chcp 65001 > NUL"); e do stdlib.h para permitir acentuação e caracteres especiais no output

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
-#include "utils.h" 
 #include <stdio.h>
+#include <stdlib.h>
+#include "utils.h" 
+
 
 int main()
 {
+    system("chcp 65001 > NUL"); //para permitir acentuação e caracteres especiais
     menuInicial();
 
     return 0;
 
 }
+
 
