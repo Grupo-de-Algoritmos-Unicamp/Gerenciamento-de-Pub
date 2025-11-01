ca5c759b9ed63266a58798e7e77017034c7d579d
Arthur Luiz Lopes Araujo <arthur.luizlopesaraujo@gmail.com>
2025-11-01 19:17:34 -0300

    feat(pedido): Adição de novas funcionalidades

--- src/pedido.h
+++ src/pedido.h
@@ -1,9 +1,14 @@
 #ifndef PEDIDO_H
 #define PEDIDO_H
 
-int processarPagamento(float valorTotal, float *pagoCliente, float *troco);
-void gerarArquivoPedido(char cpf[], int numeroPedido, char nome[][31], float preco[], int quantidade[], int i, float valorTotal, int tipoPagamento, float pagoCliente, float troco);
+int gerarProximoIDPedido();
+void gerarArquivoPedidos(int id, const char *cpf, int qtdProdutos, float total);
+void gerarArquivoItensVendidos(int id, char nomes[][31], int quant[], float unit[], float total[], int n);
+void processarPagamento(float valorTotal, float *pagoCliente, float *troco);
+int verificarProdutosPedido(int qtdProdutos, char nomes[][31], int quantidades[], float precoUnit[], float precoTotal[], float *valorTotal);
+int atualizarEstoquePedido(int qtdProdutos, char nomes[][31], int quantidades[]);
 void registrarPedido();
 
 #endif
+
 
